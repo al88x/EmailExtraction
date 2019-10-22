762f8629cc5eec0e2597bdfb627fad7f6a5a4424
Alexandru Catarau <alexctrx@gmail.com>
2019-10-22 13:55:29 +0100

    Add map implementation

--- src/alex/emailExtraction/Main.java
+++ src/alex/emailExtraction/Main.java
@@ -6,23 +6,37 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Main {
 
+    static final String PATTERN = "[a-zA-Z.'_%+-]+@([a-zA-Z.'_%+-]+\\.(?:[a-zA-Z]{2,3}|[a-zA-Z]{2}\\.[a-zA-Z]{2}))";
+
     public static void main(String[] args) throws IOException {
 
         String file = Files.readString(Paths.get("text\\sample.txt"));
-        String[] words = file.split(" ");
+        String[] words = file.split("(?: |\\r\\n)");
         int counter = 0;
-        String pattern = "[a-zA-Z.'_%+-]+@[a-zA-Z.'_%+-]+.(?:[a-zA-Z]{2,3}|[a-zA-Z]{2}.[a-zA-Z]{2})";
-
+        Map<String, Integer> domainCounterMap = new HashMap<>();
+        Pattern pattern = Pattern.compile(PATTERN);
         for (String word : words) {
-            if (Pattern.matches(pattern, word)) {
+            Matcher matcher = pattern.matcher(word);
+            if (matcher.matches()) {
+                String domain = matcher.group(1);
+                if (domainCounterMap.containsKey(domain)) {
+                    Integer counts = domainCounterMap.get(domain);
+                    domainCounterMap.replace(domain, ++counts);
+                } else {
+                    domainCounterMap.put(domain, 1);
+                }
                 counter++;
             }
         }
         System.out.println(counter);
+        System.out.println(domainCounterMap.entrySet());
     }
 }
