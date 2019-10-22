8de17f5587d4403383d2d03bceb77ffed0144c18
Alexandru Catarau <alexctrx@gmail.com>
2019-10-22 11:59:43 +0100

    Add regex processing

--- src/alex/emailExtraction/Main.java
+++ src/alex/emailExtraction/Main.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -15,8 +16,10 @@ public class Main {
         String file = Files.readString(Paths.get("text\\sample.txt"));
         String[] words = file.split(" ");
         int counter = 0;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].contains("@techsmiths.uk")) {
+        String pattern = "[a-zA-Z.'_%+-]+@[a-zA-Z.'_%+-]+.(?:[a-zA-Z]{2,3}|[a-zA-Z]{2}.[a-zA-Z]{2})";
+
+        for (String word : words) {
+            if (Pattern.matches(pattern, word)) {
                 counter++;
             }
         }
