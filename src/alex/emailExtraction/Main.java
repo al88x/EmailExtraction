9a90e00a0321b79e6af214db5b61817486a72b4e
Alexandru Catarau <alexctrx@gmail.com>
2019-10-22 11:18:05 +0100

    Implement Naive approach

--- src/alex/emailExtraction/Main.java
+++ src/alex/emailExtraction/Main.java
@@ -1,8 +1,25 @@
 package alex.emailExtraction;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        String file = Files.readString(Paths.get("text\\sample.txt"));
+        String[] words = file.split(" ");
+        int counter = 0;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].contains("@techsmiths.uk")) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
     }
 }
